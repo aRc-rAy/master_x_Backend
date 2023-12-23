5da4c6ec8af1adb3978140d488fad51048092ec0
Dilip Kumar Yadav <codingiitiandilip@gmail.com>
2023-12-23 11:12:06 +0530

    utils : asyncHandler ,ApiError, Api,reponse

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// another method------------------------====================-----------------------------------------//
+{
+  const asyncHandler = (fu) => async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      res.status(err.code || 500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  };
+}
